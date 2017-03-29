dd19ce52ed5c477cc03ae27543bf6403872cb906
Sean King <sean@seanking.org>
2017-03-29 23:30:25 +0200

    refactor test builds and travis config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,8 @@ const notify = require('gulp-notify');
 const gulpif = require('gulp-if');
 const size = require('gulp-size');
 const plumber = require('gulp-plumber');
+const lazypipe = require('lazypipe');
+const filter = require('gulp-filter');
 const gulprun = require('run-sequence');
 const yargs = require('yargs');
 const browserSync = require('browser-sync');
@@ -28,6 +30,7 @@ const json = require('rollup-plugin-json');
 // CSS
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const cssImport = require('postcss-import');
 
 const bs = browserSync.create(),
       argv = yargs.boolean(['debug']).argv,
@@ -45,6 +48,7 @@ const bs = browserSync.create(),
           format: 'iife'
         },
         postcss: [
+          cssImport(),
           autoprefixer()
         ],
         inline: {
@@ -75,42 +79,69 @@ const bs = browserSync.create(),
         }
       };
 
+const processJs = lazypipe()
+  .pipe(eslint)
+  .pipe(eslint.format)
+  .pipe(() => gulpif(!argv.debug, eslint.failAfterError()))
+  .pipe(rollup, OPTIONS.rollup);
+
 gulp.task('build', () => {
   let styles = processInline(),
       scripts = processInline();
 
   return gulp.src(['src/*.html'])
-          .pipe(errorNotifier())
+    .pipe(errorNotifier())
 
-          // Inline assets
-          .pipe(inline(OPTIONS.inline))
+    // Inline assets
+    .pipe(inline(OPTIONS.inline))
 
-          // JS
-          .pipe(scripts.extract('script'))
-            .pipe(eslint())
-            .pipe(eslint.format())
-            .pipe(gulpif(!argv.debug, eslint.failAfterError()))
-            .pipe(rollup(OPTIONS.rollup))
-          .pipe(scripts.restore())
+    // JS
+    .pipe(scripts.extract('script'))
+      .pipe(processJs())
+    .pipe(scripts.restore())
 
-          // CSS
-          .pipe(styles.extract('style'))
-            .pipe(postcss(OPTIONS.postcss))
-          .pipe(styles.restore())
+    // CSS
+    .pipe(styles.extract('style'))
+      .pipe(postcss(OPTIONS.postcss))
+    .pipe(styles.restore())
 
-          .pipe(gulpif(!argv.debug, minify(OPTIONS.HTMLmin)))
+    .pipe(gulpif(!argv.debug, minify(OPTIONS.HTMLmin)))
 
-          .pipe(size({ gzip: true }))
-        .pipe(gulp.dest('.'))
+    .pipe(size({ gzip: true }))
+  .pipe(gulp.dest('.'))
+});
+
+gulp.task('build:tests', () => {
+  const js = filter((file) => /\.(js)$/.test(file.path), { restore: true }),
+        html = filter((file) => /\.(html)$/.test(file.path), { restore: true }),
+        scripts = processInline();
+
+  return gulp.src(['test/**/*'])
+    .pipe(errorNotifier())
+
+    .pipe(html)
+      .pipe(inline(OPTIONS.inline))
+      .pipe(scripts.extract('script'))
+        .pipe(processJs())
+      .pipe(scripts.restore())
+    .pipe(html.restore)
+
+    .pipe(js)
+      .pipe(processJs())
+    .pipe(js.restore)
+
+  .pipe(gulp.dest('.test'));
 });
 
 wct.gulp.init(gulp);
 
-gulp.task('serve', (callback) => bs.init(OPTIONS.browserSync));
+gulp.task('test', () => gulprun('build:tests', 'test:local'));
+
+gulp.task('watch:src', () => gulp.watch(['src/**/*'], () => gulprun('build', 'refresh')));
+gulp.task('watch:tests', () => gulp.watch(['src/**/*', 'test/**/*'], () => gulprun('build:tests')))
+gulp.task('watch', ['watch:src', 'watch:tests']);
+
+gulp.task('serve', () => bs.init(OPTIONS.browserSync));
 gulp.task('refresh', () => bs.reload());
-
-gulp.task('test', ['build', 'test:local']);
-
-gulp.task('watch', () => gulp.watch(['src/**/*'], () => gulprun('build', 'refresh')));
 
 gulp.task('default', ['build', 'serve', 'watch']);
