e1b5757da5c63ad4e8d14923d25b18f336ab445e
Bede Overend <bede.overend@gmail.com>
2017-04-19 10:53:01 +1000

    Disable pointer events on everything but simpla elements during editing

--- src/utils/pointerEvents.js
+++ src/utils/pointerEvents.js
@@ -1,5 +1,5 @@
-const IGNORED_ELEMENTS = [ 'simpla-admin' ];
-const EVENTS_TO_BLOCK = [ 'click', 'tap' ];
+const ADMIN_ELEMENTS = [ 'simpla-admin' ];
+const EVENTS_TO_BLOCK = [ 'click', 'ontouchend' ];
 
 let elementStore = new Map(),
     observer;
@@ -15,24 +15,44 @@ function cancel(event) {
 }
 
 /**
+ * Check if given element name is Simpla name
+ * @param  {string}  name Name to check
+ * @return {Boolean}      True if name is name of a simpla element
+ */
+function isSimplaName(name) {
+  const prefixed = (str) => str.indexOf('simpla-') === 0;
+
+  return name && prefixed(name);
+}
+
+/**
+ * Check if the given element is a Simpla Element
+ * @param  {Node}  node   Node to check
+ * @return {Boolean}      True if node is a Simpla Element
+ */
+function isSimplaElement(node) {
+  if (isSimplaName(node.localName)) {
+    return true;
+  }
+
+  return node.getAttribute && isSimplaName(node.getAttribute('is'));
+}
+
+/**
  * Checks if the given node is an editable element. Editable means the element
  *  is a Simpla Element and it is not a special ignored element e.g. simpla-admin
  * @param  {Node}   node  Node to check if it's an editable element
  * @return {Boolean}      True if editable element, false otherwise
  */
 function isEditableElement(node) {
-  const prefixed = (str) => str.indexOf('simpla-') === 0,
-        ignored = (name) => IGNORED_ELEMENTS.includes(name),
-        isEditableName = (name) => name && prefixed(name) && !ignored(name);
-
-  let is;
+  const ignored = (name) => ADMIN_ELEMENTS.includes(name),
+        isEditableName = (name) => isSimplaName(name) && !ignored(name);
 
   if (isEditableName(node.localName)) {
     return true;
   }
 
-  is = node.getAttribute && node.getAttribute('is');
-  return isEditableName(is);
+  return isEditableName(node.getAttribute && node.getAttribute('is'));
 }
 
 /**
@@ -45,6 +65,15 @@ function editableElementsIn(list) {
 }
 
 /**
+ * Get all simpla elements in the given list.
+ * @param  {(NodeList|Node[])} list List of nodes to check
+ * @return {HTMLElement[]}          Array of simpla elements
+ */
+function simplaElementsIn(list) {
+  return Array.prototype.filter.call(list, isSimplaElement);
+}
+
+/**
  * Gets all elements that are currently stored and have their click events blocked
  * @param  {(NodeList|Node[])} list NodeList or Array of nodes to check
  * @return {HTMLElement[]}          Array of elements that have click listeners blocked
@@ -54,12 +83,41 @@ function blockedElementsIn(list) {
 }
 
 /**
+ * Set pointer events property on given element to given state
+ * @param  {HTMLElement}  element Element to set pointer events on
+ * @param  {string=}      state   State of pointer events
+ * @return {undefined}
+ */
+function setPointerEventsOn(element, state = '') {
+  element.style.pointerEvents = state;
+}
+
+/**
+ * Explicitly enable pointer events by setting them to 'auto'
+ * @param  {HTMLElement} element Element to set pointer events on
+ * @return {undefined}
+ */
+function enablePointerEventsOn(element) {
+  setPointerEventsOn(element, 'auto');
+}
+
+/**
+ * Reset pointer events by setting inline value to ''
+ * @param  {HTMLElement} element Element to reset pointer events on
+ * @return {undefined}
+ */
+function resetPointerEventsOn(element) {
+  setPointerEventsOn(element, '');
+}
+
+/**
  * Cancel click events on the given element
  * @param  {HTMLElement} element Element to cancel click events on
  * @return {undefined}
  */
-function cancelClicksOn(element) {
+function cancelClicksAndAllowPointer(element) {
   elementStore.set(element, 0);
+  enablePointerEventsOn(element);
   EVENTS_TO_BLOCK.forEach(event => {
     element.addEventListener(event, cancel);
   });
@@ -70,8 +128,9 @@ function cancelClicksOn(element) {
  * @param  {HTMLElement} element Element to restore click events on
  * @return {undefined}
  */
-function restoreClicksOn(element) {
+function restoreClicksAndResetPointer(element) {
   elementStore.delete(element);
+  resetPointerEventsOn(element);
   EVENTS_TO_BLOCK.forEach(event => {
     element.removeEventListener(event, cancel);
   });
@@ -85,10 +144,13 @@ function restoreClicksOn(element) {
 observer = new MutationObserver((mutations) => {
   mutations.forEach(mutation => {
     editableElementsIn(mutation.addedNodes)
-      .forEach(cancelClicksOn);
+      .forEach(cancelClicksAndAllowPointer);
+
+    simplaElementsIn(mutation.addedNodes)
+      .forEach(enablePointerEventsOn);
 
     blockedElementsIn(mutation.removedNodes)
-      .forEach(restoreClicksOn);
+      .forEach(restoreClicksAndResetPointer);
   });
 })
 
@@ -98,8 +160,9 @@ export default {
    * @return {undefined}
    */
   enable() {
-    elementStore.forEach(restoreClicksOn);
+    elementStore.forEach(restoreClicksAndResetPointer);
     observer.disconnect();
+    resetPointerEventsOn(document.body);
   },
 
   /**
@@ -107,9 +170,14 @@ export default {
    * @return {undefined}
    */
   disable() {
-    editableElementsIn(document.querySelectorAll('*'))
-      .forEach(cancelClicksOn);
+    const allElements = document.querySelectorAll('*');
+    editableElementsIn(allElements)
+      .forEach(cancelClicksAndAllowPointer);
 
+    simplaElementsIn(allElements)
+      .forEach(enablePointerEventsOn);
+
+    enablePointerEventsOn(document.body);
     observer.observe(document.body, { childList: true, subtree: true });
   }
 }
